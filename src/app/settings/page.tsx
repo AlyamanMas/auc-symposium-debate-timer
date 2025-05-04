c470961b01e97d9fe9026c8de7a828ffa28c1386
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-05-04 17:54:24 +0300

    added settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,6 +2,7 @@
 import TopAppBar from "@/ui/TopAppBar";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "@/ui/theme";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
   return (
--- src/ui/TopAppBar.tsx
+++ src/ui/TopAppBar.tsx
@@ -1,7 +1,11 @@
 import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 import Settings from "@mui/icons-material/Settings";
+import Home from "@mui/icons-material/Home";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function TopAppBar() {
+  const pathname = usePathname();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar color="transparent" elevation={0}>
@@ -10,8 +14,13 @@ export default function TopAppBar() {
           <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
             ملتقى الجامعة الأمريكية بالقاهرة
           </Typography>
-          <IconButton color="inherit" size="large">
-            <Settings />
+          <IconButton
+            color="inherit"
+            size="large"
+            href={pathname === "/settings" ? "/" : "/settings"}
+            component={Link}
+          >
+            {pathname !== "/settings" ? <Settings /> : <Home />}
           </IconButton>
         </Toolbar>
       </AppBar>
