c5ec86e9e40d7be9ee6cceffc86b842007298010
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-05-04 16:06:38 +0300

    basic top app bar with settings icon

---
+++ src/ui/TopAppBar.tsx
@@ -0,0 +1,26 @@
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import Settings from '@mui/icons-material/Settings'
+
+export default function TopAppBar() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar
+                color="transparent"
+                elevation={0}
+            >
+                <Toolbar>
+                    <Box sx={{ flexGrow: 1 }} />
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    ملتقى الجامعة الأمريكية بالقاهرة
+                    </Typography>
+                    <IconButton
+                        color="inherit"
+                        size="large"
+                    >
+                        <Settings />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
