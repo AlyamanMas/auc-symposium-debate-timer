ec42ca3a98bdaeda77d21327c0587520c82539f8
Alyaman Massarani <alyaman.maasarani@gmail.com>
2025-05-07 01:25:15 +0300

    added basic SettingsTable without operations

--- src/ui/SettingsTable.tsx
+++ src/ui/SettingsTable.tsx
@@ -1,7 +1,58 @@
 import { useAppDispatch, useAppSelector } from "@/lib/store/hooks";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { teamsMapping, statusMapping } from "@/lib/ar-mapping";
+import { formatTime } from "@/lib/utils";
+
+const headLabelList = ["العنوان", "الفريق", "المدة", "الحالة", "العمليات"];
 
 export default function SettingsTable() {
   const dispatch = useAppDispatch();
   const debate_state = useAppSelector((state) => state.debate);
-  return <div>{JSON.stringify(debate_state)}</div>;
+  return (
+    <Paper variant="outlined">
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {headLabelList.map((h) => (
+                <TableCell>
+                  <Typography fontWeight={500}>{h}</Typography>
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {debate_state.sections.map((section) => (
+              <TableRow key={section.id}>
+                <TableCell>
+                  <Typography>{section.name}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>
+                    {teamsMapping.find((x) => x.value === section.team)?.label}
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{formatTime(section.duration)}</Typography>
+                </TableCell>
+                <TableCell>
+                  <Typography>{statusMapping[section.status]}</Typography>
+                </TableCell>
+                <TableCell>TODO</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
+  );
 }
